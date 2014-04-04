dab5bfe2faa8e2e2c6d501eebcfb915b4a408a4a
pepsi7959 <pepsi_kmutnb@windowslive.com>
2014-04-04 15:35:11 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,10 @@
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
+#include "color.h"
 
+#define LINUX//!use linux OS
+int main(){
+    (void)print_string("pepsi\n",TXT_RED);
+    (void)print_string("red\n",TXT_GREEN);
 return 0;
 }
